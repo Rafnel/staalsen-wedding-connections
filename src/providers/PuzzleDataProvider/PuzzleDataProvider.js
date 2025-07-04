7ba65cf14b5719b52e994a532bbf2a0257f55daa
Zachary Steudel <zacshack174@gmail.com>
2025-07-04 12:41:44 -0500

    Put connections game in PuzzleDataProvider

--- src/providers/PuzzleDataProvider/PuzzleDataProvider.js
+++ src/providers/PuzzleDataProvider/PuzzleDataProvider.js
@@ -1,5 +1,31 @@
 import React from "react";
-import { CONNECTION_GAMES } from "../../lib/data";
+
+export const CONNECTION_GAMES = [
+  [
+    {
+      category: "Shared Hobbies",
+      words: ["Board", "Disc", "Pickle", "Ultimate"],
+      difficulty: 1,
+    },
+    {
+      category: "Places to Visit in Costa Rica (Our Honeymoon!)",
+      words: ["Ocean", "Rainforest", "Springs", "Volcano"],
+      difficulty: 2,
+    },
+    {
+      category: "Parts of Titles to Our Favorite Movies",
+      words: ["Monty", "Pirates", "Pride", "Princess"],
+      difficulty: 3,
+    },
+
+    {
+      category: "Unity Symbolism During Wedding Ceremony",
+      words: ["Candle", "Knot", "Ring", "Sand"],
+      difficulty: 4,
+    },
+  ],
+];
+
 
 export const PuzzleDataContext = React.createContext();
 
